aec3b8df40fa9ab3c2333b4f1ec97ad6c88d91e1
Anos Voaldigoad <83455762+Anos-Voldigoad12@users.noreply.github.com>
2023-06-10 18:47:33 +0530

    Update String-Reverse.c

--- Assignments/String-Reverse.c
+++ Assignments/String-Reverse.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
-char* reverseString(const char str[])
+void reverseString(const char str[], char rev[128])
 {
-  char rev[128];
   int i,j=0,len;
   for(i=0;str[i]!='\0';i++)
     len++;
   for(i=len-1;i>=0;i--)
     rev[j++] = str[i];
   rev[j] = '\0';
-  return rev;
 }
 int main()
 {
   char str[128],rev[128];
   printf("Enter a String: "); scanf("%[^\n]",str);
-  rev = reverseString(str);
+  reverseString(str,rev);
   printf("Reversed String: %s\n",rev);
 }
